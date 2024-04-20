c3b93723eecc63364fc2e83e8510a0f595797f84
Wesley00s <wesley300rodrigues@gmail.com>
2024-04-20 11:50:30 -0300

    Commit init

---
+++ src/test/java/org/wesley/ecommerce/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wesley.ecommerce.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

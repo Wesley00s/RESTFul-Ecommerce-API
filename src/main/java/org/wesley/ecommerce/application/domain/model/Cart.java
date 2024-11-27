30384f1033c2c21dbc636cf64c46e8d4ff49a2d9
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-27 18:41:25 -0300

    refcator: change cart class model structure

--- src/main/java/org/wesley/ecommerce/application/domain/model/Cart.java
+++ src/main/java/org/wesley/ecommerce/application/domain/model/Cart.java
@@ -1,29 +1,21 @@
 package org.wesley.ecommerce.application.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Entity(name = "cart")
+@Data
+@Entity
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
     @JoinColumn(name = "user_id")
-    @JsonBackReference
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    private List<Product> products;
-    private boolean isActive;
+    private Users users;
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+    private Double totalPrice = 0.0;
 }

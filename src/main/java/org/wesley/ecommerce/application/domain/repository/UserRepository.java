061fb7312c8af8f428f80a71eac227632db6836a
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:32:19 -0300

    refactor: change users class name

--- src/main/java/org/wesley/ecommerce/application/domain/repository/UserRepository.java
+++ src/main/java/org/wesley/ecommerce/application/domain/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package org.wesley.ecommerce.application.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.wesley.ecommerce.application.domain.model.User;
+import org.wesley.ecommerce.application.domain.model.Users;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<Users, UUID> {
+    Optional<Users> findByEmail(String email);
 }

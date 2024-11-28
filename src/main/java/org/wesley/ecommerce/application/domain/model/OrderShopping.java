4a543f43e93d0d8d675bc32df4c6c60b09ffad9b
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:24:28 -0300

    refactor: remove explicty entity name

--- src/main/java/org/wesley/ecommerce/application/domain/model/OrderShopping.java
+++ src/main/java/org/wesley/ecommerce/application/domain/model/OrderShopping.java
@@ -1,29 +1,23 @@
 package org.wesley.ecommerce.application.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 import org.wesley.ecommerce.application.domain.enumeration.OrderStatus;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@Entity(name = "order_shopping")
-public class Order {
+@Data
+@Entity
+public class OrderShopping {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JoinColumn(name = "cart_id")
     private Cart cart;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
     private OrderStatus status;
 

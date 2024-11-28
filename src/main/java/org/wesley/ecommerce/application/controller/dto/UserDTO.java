85e9299d8a788cd65b49c5c4a57b97f56c29ebe2
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:32:14 -0300

    refactor: change users class name

--- src/main/java/org/wesley/ecommerce/application/controller/dto/UserDTO.java
+++ src/main/java/org/wesley/ecommerce/application/controller/dto/UserDTO.java
@@ -2,7 +2,7 @@ package org.wesley.ecommerce.application.controller.dto;
 
 import org.wesley.ecommerce.application.domain.enumeration.UserType;
 import org.wesley.ecommerce.application.domain.model.Address;
-import org.wesley.ecommerce.application.domain.model.User;
+import org.wesley.ecommerce.application.domain.model.Users;
 
 import java.time.LocalDateTime;
 
@@ -18,34 +18,35 @@ public record UserDTO(
         LocalDateTime createdAt
 ) {
 
-    public static UserDTO fromUser(User user) {
+    public static UserDTO fromUser(Users users) {
         return new UserDTO(
-                user.getName(),
-                user.getEmail(),
-                user.getPassword(),
-                user.getUserType(),
-                user.getAddress().getStreet(),
-                user.getAddress().getCity(),
-                user.getAddress().getState(),
-                user.getAddress().getZip(),
-                user.getCreatedAt()
+                users.getName(),
+                users.getEmail(),
+                users.getPassword(),
+                users.getUserType(),
+                users.getAddress().getStreet(),
+                users.getAddress().getCity(),
+                users.getAddress().getState(),
+                users.getAddress().getZip(),
+                users.getCreatedAt()
         );
     }
 
-    public User from() {
+    public Users from() {
         var address = new Address();
         address.setStreet(street());
         address.setCity(city());
         address.setState(state());
         address.setZip(zip());
 
-        var usr = new User();
+        var usr = new Users();
         usr.setName(name());
         usr.setEmail(email());
         usr.setPassword(password());
         usr.setUserType(userType());
         usr.setAddress(address);
         usr.setCreatedAt(createdAt);
+
         return usr;
     }
 }

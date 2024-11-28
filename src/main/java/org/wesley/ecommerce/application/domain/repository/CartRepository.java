23aa3826ba7fee45a35517b312d398c2289bd7f7
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:16:53 -0300

    refactor: remove transactional methods

--- src/main/java/org/wesley/ecommerce/application/domain/repository/CartRepository.java
+++ src/main/java/org/wesley/ecommerce/application/domain/repository/CartRepository.java
@@ -1,8 +1,6 @@
 package org.wesley.ecommerce.application.domain.repository;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.wesley.ecommerce.application.domain.model.Cart;
 
@@ -10,17 +8,7 @@ import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    @Query(value = "SELECT * FROM cart AS c WHERE c.user_id = ?1 AND c.id = ?2",
+    @Query(value = "SELECT * FROM cart WHERE user_id = ?1",
             nativeQuery = true)
-    Cart findCartByUserId(UUID userId, Long cartId);
-
-    @Transactional
-    @Modifying
-    @Query(value = "UPDATE cart SET is_active = false WHERE id = ?1", nativeQuery = true)
-    void disableCart(Long cartId);
-
-    @Modifying
-    @Transactional
-    @Query(value = "INSERT INTO cart_products (cart_id, products_id) VALUES (?1, ?2)", nativeQuery = true)
-    void addToCart(Long cartId, Long productId);
+    Cart findCartByUserId(UUID userId);
 }

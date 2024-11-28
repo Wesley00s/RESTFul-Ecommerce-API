21d42c6150dc1f250f9c1ea84520e447909bddf5
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:27:05 -0300

    refactor: remove unneccessary method

--- src/main/java/org/wesley/ecommerce/application/domain/repository/ProductRepository.java
+++ src/main/java/org/wesley/ecommerce/application/domain/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package org.wesley.ecommerce.application.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.wesley.ecommerce.application.domain.model.Product;
 
@@ -9,11 +8,6 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Modifying
-    @Query(value = "UPDATE product SET stock_quantity = ?2 WHERE id = ?1",
-            nativeQuery = true)
-    void updateQuantityStock(Long productId, int quantity);
-
-    @Query(value = "SELECT p.* FROM product AS p JOIN cart_products AS cp ON p.id = cp.products_id WHERE cp.cart_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT p.* FROM product AS p JOIN cart_item AS ci ON p.id = ci.product_id WHERE ci.cart_id = ?1", nativeQuery = true)
     List<Product> findProductsByCart(Long cartId);
 }

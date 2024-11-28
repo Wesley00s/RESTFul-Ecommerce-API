acc67fc295975e42fca106401d9457a94c60cb4c
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:14:02 -0300

    refcator: refactor cart controller

--- src/main/java/org/wesley/ecommerce/application/controller/CartController.java
+++ src/main/java/org/wesley/ecommerce/application/controller/CartController.java
@@ -2,101 +2,145 @@ package org.wesley.ecommerce.application.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import org.wesley.ecommerce.application.controller.dto.CartDTO;
-import org.wesley.ecommerce.application.domain.model.Cart;
-import org.wesley.ecommerce.application.domain.model.Product;
+import org.wesley.ecommerce.application.service.CartItemService;
 import org.wesley.ecommerce.application.service.CartService;
 import org.wesley.ecommerce.application.service.ProductService;
-import org.wesley.ecommerce.application.service.UserService;
+import org.wesley.ecommerce.application.service.implement.AuthenticationService;
 
 @RestController
 @RequestMapping("/cart")
-@RequiredArgsConstructor
+@Data
 @Tag(name = "Cart Item Controller", description = "RESTFul API for managing cart items.")
 public class CartController {
 
     private final CartService cartService;
-    private final UserService userService;
+    private final CartItemService cartItemService;
     private final ProductService productService;
+    private final AuthenticationService authenticationService;
 
-    @Operation(summary = "Create a new cart item", description = "Creates a new cart for the authenticated user.")
+    @Operation(summary = "Add product from cart", description = "Add product from existing cart")
     @PostMapping
-    public ResponseEntity<String> createCartItem() {
-        var authenticatedUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        var authenticatedUserId = userService.findByEmail(authenticatedUser.getUsername()).getUserId();
+    public ResponseEntity<String> addProductFromCart(
+            @RequestParam("product") Long productId,
+            @RequestParam("quantity") Integer quantity) {
+        var user = authenticationService.getAuthenticatedUser();
+        var cart = authenticationService.getActiveCart(user);
 
-        var user = userService.findById(authenticatedUserId);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User not found");
+        var product = productService.findById(productId);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
 
-        var cart = new Cart();
-        cart.setUser(user);
-        cart.setActive(true);
-        cartService.create(cart);
+        if (!productService.isStockAvailable(productId, quantity)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Product is not available");
+        }
 
-        return ResponseEntity.ok("Cart item created.");
+        var existingCartItem = cart.getItems().stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst();
+
+        var totalPrice = product.getPrice() * quantity;
+
+        if (existingCartItem.isPresent()) {
+            var cartItem = existingCartItem.get();
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+
+            cart.setTotalPrice(cart.getTotalPrice() + totalPrice);
+            cartService.update(cart.getId(), cart);
+            cart.getItems().forEach(
+                    it -> cartItemService.updateTotalPrice(cart.getTotalPrice(),
+                            it.getProduct().getId(), cart.getId()));
+
+            return ResponseEntity.ok("Product quantity updated in cart successfully.");
+        } else {
+            cartItemService.addToCartItem(cart.getId(), productId, quantity, totalPrice);
+
+            cart.setTotalPrice(cart.getTotalPrice() + totalPrice);
+            cartService.update(cart.getId(), cart);
+
+            return ResponseEntity.ok("Product added to cart successfully.");
+        }
     }
 
-    @Operation(summary = "Add product to cart", description = "Add product to existing cart")
-    @PostMapping("/add/{cartId}/{productId}")
-    public ResponseEntity<String> addProductToCart(@PathVariable Long cartId, @PathVariable Long productId) {
-        var authenticatedUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        var authenticatedUserId = userService.findByEmail(authenticatedUser.getUsername()).getUserId();
-        var user = userService.findById(authenticatedUserId);
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User not found");
-        }
-        var cart = cartService.findById(cartId);
-        Product product = new Product();
-
-        productService.findAll().forEach(e -> {
-            if(e.getId().equals(productId)) {
-                product.setId(e.getId());
-                product.setName(e.getName());
-                product.setPrice(e.getPrice());
-                product.setDescription(e.getDescription());
-            }
-        });
+    @Operation(summary = "Remove all products references from cart", description = "Remove all product references from existing cart")
+    @DeleteMapping
+    public ResponseEntity<String> removeAllProductsReferencesFromCart(
+            @RequestParam("product") Long productId) {
+        var user = authenticationService.getAuthenticatedUser();
+        var cart = authenticationService.getActiveCart(user);
 
         if (cart == null) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Cart not found");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User does not have an active cart.");
         }
-        cartService.addToCart(cartId, productId);
-        return ResponseEntity.ok("Cart item added.");
+        var product = productService.findById(productId);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        cartItemService.removeAllFromCartItem(cart.getId(), productId);
+        cart.getItems().removeIf(cartItem -> productId.equals(cartItem.getProduct().getId()));
+
+        double totalPrice = cart.getItems().stream()
+                .map(item -> item.getProduct().getPrice() * item.getQuantity())
+                .reduce(0.0, Double::sum);
+        cart.setTotalPrice(totalPrice);
+
+        cartService.update(cart.getId(), cart);
+
+
+        return ResponseEntity.ok("Product removed from cart successfully.");
+
     }
 
-    @Operation(summary = "Retrieve a cart by ID", description = "Gets the details of a cart by its ID, for the authenticated user.")
-    @GetMapping("/{cartId}")
-    public ResponseEntity<CartDTO> getCartItem(@PathVariable Long cartId) {
-        var authenticatedUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        var authenticatedUserId = userService.findByEmail(authenticatedUser.getUsername()).getUserId();
+    @Operation(summary = "Remove one quantity products references from cart", description = "Remove one quantity product references from existing cart")
+    @PutMapping
+    public ResponseEntity<String> removeOnlyProductReferencesFromCart(
+            @RequestParam("product") Long productId,
+            @RequestParam("quantity") int quantity
+    ) {
+        var user = authenticationService.getAuthenticatedUser();
+        var cart = authenticationService.getActiveCart(user);
 
-        if (cartService.findCartByUserId(authenticatedUserId, cartId) == null) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-
+        if (cart == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User does not have an active cart.");
         }
-        Cart cart = cartService.findCartByUserId(authenticatedUserId, cartId);
 
-        return ResponseEntity.ok(new CartDTO(authenticatedUserId, cart.isActive()));
-    }
-
-    @Operation(summary = "Disable a cart", description = "Disables a cart by its ID for the authenticated user.")
-    @PutMapping("/disabled/{cartId}")
-    public ResponseEntity<String> disableCartItem(@PathVariable Long cartId) {
-        var authenticatedUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        var authenticatedUserId = userService.findByEmail(authenticatedUser.getUsername()).getUserId();
-
-        if (cartService.findCartByUserId(authenticatedUserId, cartId) == null) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have access to this cart.");
+        var product = productService.findById(productId);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
-        cartService.disableCart(cartId);
-        return ResponseEntity.ok("Cart item was disabled.");
+
+        var cartItem = cart.getItems().stream()
+                .filter(item -> productId.equals(item.getProduct().getId()))
+                .findFirst();
+
+        if (cartItem.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product is not in the cart.");
+        }
+
+        if (cartItem.get().getQuantity() < quantity) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You cannot remove more products than are in the cart.");
+        }
+
+        cartItemService.removeOnlyFromCartItem(productId, cart.getId(), quantity);
+        var item = cartItem.get();
+        item.setQuantity(item.getQuantity() - quantity);
+
+        if (item.getQuantity() == 0) {
+            cart.getItems().remove(item);
+        }
+
+        double newTotalPrice = cart.getItems().stream()
+                .mapToDouble(it -> it.getProduct().getPrice() * it.getQuantity())
+                .sum();
+        cart.setTotalPrice(newTotalPrice);
+        cartService.update(cart.getId(), cart);
+        cart.getItems().forEach(
+                it -> cartItemService.updateTotalPrice(cart.getTotalPrice(),
+                        it.getProduct().getId(), cart.getId()));
+        return ResponseEntity.ok(quantity + " " + product.getName() + " removed from cart successfully.");
     }
 }

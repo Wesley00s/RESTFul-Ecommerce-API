950bce26b4490d9c70fb13e1a72bf059fc320989
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:29:42 -0300

    refactor: improve authorization routes and implement handle methods

--- src/main/java/org/wesley/ecommerce/application/config/security/SecurityConfig.java
+++ src/main/java/org/wesley/ecommerce/application/config/security/SecurityConfig.java
@@ -1,8 +1,9 @@
 package org.wesley.ecommerce.application.config.security;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,27 +14,40 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.wesley.ecommerce.application.exceptions.CustomAccessDeniedHandler;
+import org.wesley.ecommerce.application.exceptions.CustomAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+@Data
 public class SecurityConfig {
 
     private final SecurityFilter securityFilter;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth", "/register", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/product/**").authenticated()
+                        .requestMatchers("/cart/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/product/**", "/cart/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/product/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/product/**").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/product/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
+
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptions -> exceptions
+                        .accessDeniedHandler(accessDeniedHandler)
+                        .authenticationEntryPoint(authenticationEntryPoint));
 
         return http.build();
     }

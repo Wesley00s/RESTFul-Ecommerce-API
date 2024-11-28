fd1b24d717ab4f7bcbc54bf14be8cba6842645da
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:27:50 -0300

    refactor: add missing attributes

--- src/main/java/org/wesley/ecommerce/application/controller/dto/ProductResponseDTO.java
+++ src/main/java/org/wesley/ecommerce/application/controller/dto/ProductResponseDTO.java
@@ -3,34 +3,40 @@ package org.wesley.ecommerce.application.controller.dto;
 import org.wesley.ecommerce.application.domain.enumeration.ProductCategory;
 import org.wesley.ecommerce.application.domain.model.Product;
 
-import java.math.BigDecimal;
-
 import static org.wesley.ecommerce.application.controller.dto.ProductRequestDTO.getProduct;
 
 
 public record ProductResponseDTO(
-        String code,
         String name,
-        String description,
-        BigDecimal price,
-        Integer stockQuantity,
         String imageUrl,
-        ProductCategory category
+        String code,
+        String description,
+        Integer stock,
+        ProductCategory category,
+        Double price
 ) {
 
     public static ProductResponseDTO fromDTO(Product product) {
         return new ProductResponseDTO(
-                product.getCode(),
                 product.getName(),
-                product.getDescription(),
-                product.getPrice(),
-                product.getStockQuantity(),
                 product.getImageUrl(),
-                product.getCategory()
+                product.getCode(),
+                product.getDescription(),
+                product.getStock(),
+                product.getCategory(),
+                product.getPrice()
         );
     }
 
     public Product from(String code) {
-        return getProduct(code, this.name, this.description, this.price, this.stockQuantity, this.imageUrl, this.category);
+        return getProduct(
+                this.name,
+                this.imageUrl,
+                code,
+                this.description,
+                this.stock,
+                this.category,
+                this.price
+        );
     }
 }

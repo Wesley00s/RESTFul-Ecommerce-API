360f67be6f05a5a29cb979a155555dcaf95c4af8
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:15:41 -0300

    feat: create class cart item

---
+++ src/main/java/org/wesley/ecommerce/application/domain/model/CartItem.java
@@ -0,0 +1,23 @@
+package org.wesley.ecommerce.application.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "cart_id", referencedColumnName = "id")
+    private Cart cart;
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+    @Column(nullable = false)
+    private Integer quantity;
+    @Column(nullable = false)
+    private Double price;
+}
+

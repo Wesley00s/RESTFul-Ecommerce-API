c6fd98e665784111a29f34e09e6093c2abcacddb
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:25:32 -0300

    refactor: remove explicty entity name and include new attributes

--- src/main/java/org/wesley/ecommerce/application/domain/model/Product.java
+++ src/main/java/org/wesley/ecommerce/application/domain/model/Product.java
@@ -1,29 +1,25 @@
 package org.wesley.ecommerce.application.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 import org.wesley.ecommerce.application.domain.enumeration.ProductCategory;
 
-import java.math.BigDecimal;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Entity(name = "product")
+@Data
+@Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String code;
     private String name;
+    private String imageUrl;
+    @Column(unique = true)
+    private String code;
     @Column(length = 10000)
     private String description;
-    private BigDecimal price;
-    private Integer stockQuantity;
-    private String imageUrl;
+    private Integer stock;
+    @Enumerated(EnumType.STRING)
     private ProductCategory category;
+    @Column(nullable = false)
+    private Double price;
 }
+

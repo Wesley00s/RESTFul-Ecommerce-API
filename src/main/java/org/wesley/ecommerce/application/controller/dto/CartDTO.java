1089c8c5bb8c8a7b4425a7735cf2190bb7315d29
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-28 16:15:01 -0300

    refactor: remove activation method to cart dto

--- src/main/java/org/wesley/ecommerce/application/controller/dto/CartDTO.java
+++ src/main/java/org/wesley/ecommerce/application/controller/dto/CartDTO.java
@@ -1,29 +1,31 @@
 package org.wesley.ecommerce.application.controller.dto;
 
 import org.wesley.ecommerce.application.domain.model.Cart;
-import org.wesley.ecommerce.application.domain.model.User;
+import org.wesley.ecommerce.application.domain.model.CartItem;
+import org.wesley.ecommerce.application.domain.model.Users;
 
+import java.util.List;
 import java.util.UUID;
 
 
 public record CartDTO(
         UUID userId,
-        boolean isActive
+        List<CartItem> items,
+        Double totalPrice
 ) {
 
     public static CartDTO fromDTO(Cart cart) {
         return new CartDTO(
-                cart.getUser().getUserId(),
-                cart.isActive()
+                cart.getUsers().getId(),
+                cart.getItems(),
+                cart.getTotalPrice()
         );
     }
 
     public Cart from() {
         var cart = new Cart();
-        var user = new User();
-        cart.setUser(user);
-        cart.setActive(isActive);
-        user.getCarts().add(cart);
+        var user = new Users();
+        cart.setUsers(user);
         return cart;
     }
 }

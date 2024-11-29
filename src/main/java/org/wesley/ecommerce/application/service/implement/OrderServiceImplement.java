f2b74e79891a9d209a9f7de941b696b487bd1fd9
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-29 19:45:50 -0300

    feat: implement update method on order service

--- src/main/java/org/wesley/ecommerce/application/service/implement/OrderServiceImplement.java
+++ src/main/java/org/wesley/ecommerce/application/service/implement/OrderServiceImplement.java
@@ -6,6 +6,7 @@ import org.wesley.ecommerce.application.domain.repository.OrderRepository;
 import org.wesley.ecommerce.application.service.OrderService;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class OrderServiceImplement implements OrderService {
@@ -20,9 +21,30 @@ public class OrderServiceImplement implements OrderService {
         return orderRepository.save(orderShopping);
     }
 
-
     @Override
     public OrderShopping findById(Long id) {
         return orderRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public void update(Long orderId, OrderShopping orderShopping) {
+        Optional<OrderShopping> order = orderRepository.findById(orderId);
+        if(order.isPresent()) {
+            var existingOrder = order.get();
+            if(existingOrder.getStatus() != null) {
+                existingOrder.setStatus(orderShopping.getStatus());
+            }
+            if(orderShopping.getId() != null) {
+                existingOrder.setId(orderShopping.getId());
+            }
+            if(orderShopping.getCart() != null) {
+                existingOrder.setCart(orderShopping.getCart());
+            }
+            if(orderShopping.getCreatedAt() != null) {
+                existingOrder.setCreatedAt(orderShopping.getCreatedAt());
+            }
+        } else {
+            throw new NoSuchElementException("Order not found for update");
+        }
     }
 }

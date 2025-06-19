cca4ebd5235e7ea72a4b6601797d4d954f58517d
Wesley00s <wesley300rodrigues@gmail.com>
2025-06-18 21:01:35 -0300

    refactor: rename environment variables

--- src/main/java/org/wesley/ecommerce/application/config/DotenvConfig.java
+++ src/main/java/org/wesley/ecommerce/application/config/DotenvConfig.java
@@ -12,11 +12,11 @@ public class DotenvConfig {
     @PostConstruct
     public static void loadDotenv() {
         Dotenv dotenv = Dotenv.configure().load();
-        System.setProperty("DATABASE_HOST", Objects.requireNonNull(dotenv.get("DATABASE_HOST")));
-        System.setProperty("DATABASE_NAME", Objects.requireNonNull(dotenv.get("DATABASE_NAME")));
-        System.setProperty("DATABASE_PORT", Objects.requireNonNull(dotenv.get("DATABASE_PORT")));
-        System.setProperty("DATABASE_USER", Objects.requireNonNull(dotenv.get("DATABASE_USER")));
-        System.setProperty("DATABASE_PASSWORD", Objects.requireNonNull(dotenv.get("DATABASE_PASSWORD")));
+        System.setProperty("POSTGRES_HOST", Objects.requireNonNull(dotenv.get("POSTGRES_HOST")));
+        System.setProperty("POSTGRES_DB", Objects.requireNonNull(dotenv.get("POSTGRES_DB")));
+        System.setProperty("POSTGRES_PORT", Objects.requireNonNull(dotenv.get("POSTGRES_PORT")));
+        System.setProperty("POSTGRES_USER", Objects.requireNonNull(dotenv.get("POSTGRES_USER")));
+        System.setProperty("POSTGRES_PASSWORD", Objects.requireNonNull(dotenv.get("POSTGRES_PASSWORD")));
         System.setProperty("PUBLIC_KEY", Objects.requireNonNull(dotenv.get("PUBLIC_KEY")));
         System.setProperty("PRIVATE_KEY", Objects.requireNonNull(dotenv.get("PRIVATE_KEY")));
         System.setProperty("ISSUER", Objects.requireNonNull(dotenv.get("ISSUER")));

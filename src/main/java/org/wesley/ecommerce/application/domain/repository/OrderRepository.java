7a2994791042894f94d1938d5dc43e38917acd96
Wesley00s <wesley300rodrigues@gmail.com>
2025-06-22 19:49:48 -0300

    feat: add new methods

--- src/main/java/org/wesley/ecommerce/application/domain/repository/OrderRepository.java
+++ src/main/java/org/wesley/ecommerce/application/domain/repository/OrderRepository.java
@@ -1,7 +1,15 @@
 package org.wesley.ecommerce.application.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.wesley.ecommerce.application.domain.model.OrderShopping;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface OrderRepository extends JpaRepository<OrderShopping, Long> {
+    @Query("SELECT o FROM OrderShopping o JOIN FETCH o.items")
+    List<OrderShopping> findAllWithItems();
+
+    List<OrderShopping> findByUserIdOrderByCreatedAtDesc(UUID userId);
 }

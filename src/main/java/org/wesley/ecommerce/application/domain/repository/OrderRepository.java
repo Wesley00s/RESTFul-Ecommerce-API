b9b2f3ec4e195b98307b1b6b7165feab7dace5d1
Wesley00s <wesley300rodrigues@gmail.com>
2025-07-19 23:17:24 -0300

    refactor: refactor pagination

--- src/main/java/org/wesley/ecommerce/application/domain/repository/OrderRepository.java
+++ src/main/java/org/wesley/ecommerce/application/domain/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package org.wesley.ecommerce.application.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.wesley.ecommerce.application.domain.model.OrderShopping;
@@ -8,8 +10,11 @@ import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<OrderShopping, Long> {
-    @Query("SELECT o FROM OrderShopping o JOIN FETCH o.items")
-    List<OrderShopping> findAllWithItems();
+    @Query(
+            value = "SELECT o FROM OrderShopping o LEFT JOIN FETCH o.items",
+            countQuery = "SELECT COUNT(o) FROM OrderShopping o"
+    )
+    Page<OrderShopping> findAllWithItems(Pageable pageable);
 
     List<OrderShopping> findByUserIdOrderByCreatedAtDesc(UUID userId);
 }

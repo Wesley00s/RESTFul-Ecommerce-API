67fd7611d29da2092dc797dd57366a2fa5046efb
Wesley00s <wesley300rodrigues@gmail.com>
2025-07-19 23:18:01 -0300

    feat: declare find products by category

--- src/main/java/org/wesley/ecommerce/application/domain/repository/ProductRepository.java
+++ src/main/java/org/wesley/ecommerce/application/domain/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package org.wesley.ecommerce.application.domain.repository;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.wesley.ecommerce.application.domain.enumeration.ProductCategory;
 import org.wesley.ecommerce.application.domain.model.Product;
 
 import java.util.List;
@@ -10,4 +14,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query(value = "SELECT p.* FROM product AS p JOIN cart_item AS ci ON p.id = ci.product_id WHERE ci.cart_id = ?1", nativeQuery = true)
     List<Product> findProductsByCart(Long cartId);
+
+    Page<Product> findProductsByCategory(ProductCategory category, Pageable pageable);
 }

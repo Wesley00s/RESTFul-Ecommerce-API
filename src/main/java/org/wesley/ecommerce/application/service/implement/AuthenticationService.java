29440c1fe8f3e92fe8e177dea7e362b754e5882a
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-12 18:45:05 -0300

    refactor: inject UserRepository in AuthenticationService

--- src/main/java/org/wesley/ecommerce/application/service/implement/AuthenticationService.java
+++ src/main/java/org/wesley/ecommerce/application/service/implement/AuthenticationService.java
@@ -1,25 +1,25 @@
 package org.wesley.ecommerce.application.service.implement;
-
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.wesley.ecommerce.application.domain.model.Cart;
 import org.wesley.ecommerce.application.domain.model.Users;
-import org.wesley.ecommerce.application.service.UserService;
+import org.wesley.ecommerce.application.domain.repository.UserRepository;
 
 @Service
+@AllArgsConstructor
 public class AuthenticationService {
 
-    private final UserService userService;
-
-    public AuthenticationService(UserService userService) {
-        this.userService = userService;
-    }
+    private final UserRepository userRepository;
 
     public Users getAuthenticatedUser() {
         var authenticatedUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        var authenticatedUserId = userService.findByEmail(authenticatedUser.getUsername()).getId();
-        return userService.findById(authenticatedUserId);
+        var authenticatedUserId = userRepository.findByEmail(authenticatedUser.getUsername())
+                .orElseThrow(() -> new IllegalStateException("User is not authenticated"))
+                .getId();
+        return userRepository.findById(authenticatedUserId)
+                .orElseThrow(() -> new IllegalStateException("User is not authenticated"));
     }
 
     public Cart getActiveCart(Users user) {

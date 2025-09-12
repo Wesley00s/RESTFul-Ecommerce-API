e4ca442b810e765e900bed60b16b049017538652
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-12 18:44:28 -0300

    feat: enhance Product model with new fields

--- src/main/java/org/wesley/ecommerce/application/domain/model/Product.java
+++ src/main/java/org/wesley/ecommerce/application/domain/model/Product.java
@@ -6,6 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.wesley.ecommerce.application.domain.enumeration.ProductCategory;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,7 +19,14 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String imageUrl;
+    private String coverImageUrl;
+    @ElementCollection
+    @CollectionTable(
+            name = "product_image_urls",
+            joinColumns = @JoinColumn(name = "product_id")
+    )
+    @Column(name = "image_url")
+    private List<String> imageUrls = new ArrayList<>();
     @Column(unique = true)
     private String code;
     @Column(length = 10000)
@@ -25,5 +36,18 @@ public class Product {
     private ProductCategory category;
     @Column(nullable = false)
     private Double price;
+    @Column(nullable = false, columnDefinition = "default 0")
+    private Double rating = 0.0;
+    @Column(nullable = false, columnDefinition = "default 0")
+    private Integer soldCount = 0;
+    @Column(nullable = false)
+    private Boolean isAvailable = true;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
 

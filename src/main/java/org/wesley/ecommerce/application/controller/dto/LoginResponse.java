14536cdf4a561411e30aeec74e3628dc44a65683
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-12 18:42:22 -0300

    refactor: remove unused DTOs

--- src/main/java/org/wesley/ecommerce/application/controller/dto/LoginResponse.java
+++
@@ -1,5 +0,0 @@
-package org.wesley.ecommerce.application.controller.dto;
-
-public record LoginResponse(String name, String token, String userType) {
-
-}
--- src/main/java/org/wesley/ecommerce/application/controller/dto/RegisterResponse.java
+++
@@ -1,5 +0,0 @@
-package org.wesley.ecommerce.application.controller.dto;
-
-public record RegisterResponse(String message) {
-
-}

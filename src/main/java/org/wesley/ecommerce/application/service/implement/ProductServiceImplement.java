492960e4380e018dbd53985ebcc77c6f139da3a4
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-12 18:45:52 -0300

    refactor: update product service for enhanced filtering and sorting

--- src/main/java/org/wesley/ecommerce/application/service/implement/ProductServiceImplement.java
+++ src/main/java/org/wesley/ecommerce/application/service/implement/ProductServiceImplement.java
@@ -1,10 +1,15 @@
 package org.wesley.ecommerce.application.service.implement;
 
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.wesley.ecommerce.application.domain.enumeration.ProductCategory;
+import org.wesley.ecommerce.application.domain.enumeration.ProductSortBy;
+import org.wesley.ecommerce.application.domain.enumeration.SortDirection;
 import org.wesley.ecommerce.application.domain.model.Product;
+import org.wesley.ecommerce.application.domain.repository.CartRepository;
 import org.wesley.ecommerce.application.domain.repository.ProductRepository;
 import org.wesley.ecommerce.application.service.ProductService;
 
@@ -13,12 +18,10 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class ProductServiceImplement implements ProductService {
     final private ProductRepository productRepository;
-
-    public ProductServiceImplement(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+    private final CartRepository cartRepository;
 
     @Override
     public Product create(Product product) {
@@ -39,8 +42,23 @@ public class ProductServiceImplement implements ProductService {
     }
 
     @Override
-    public Page<Product> findAll(Integer page, Integer pageSize) {
-        return productRepository.findAll(PageRequest.of(page, pageSize));
+    public Page<Product> findAll(ProductSortBy sortBy, SortDirection sortDirection, String name, Integer page, Integer pageSize) {
+
+        var finalSortBy = (sortBy == null) ? ProductSortBy.NAME : sortBy;
+        Sort.Direction finalDirection = (sortDirection == null || sortDirection == SortDirection.ASC)
+                ? Sort.Direction.ASC
+                : Sort.Direction.DESC;
+
+        var pageable = PageRequest.of(
+                page,
+                pageSize,
+                Sort.by(finalDirection, finalSortBy.getPropertyName())
+        );
+
+        if (name != null && !name.isBlank() && !name.trim().isEmpty()) {
+            return productRepository.findByNameContainingIgnoreCase(name, pageable);
+        }
+        return productRepository.findAll(pageable);
     }
 
     @Override
@@ -67,8 +85,11 @@ public class ProductServiceImplement implements ProductService {
             if (product.getPrice() != null) {
                 productExist.setPrice(product.getPrice());
             }
-            if (product.getImageUrl() != null) {
-                productExist.setImageUrl(product.getImageUrl());
+            if (product.getCoverImageUrl() != null) {
+                productExist.setCoverImageUrl(product.getCoverImageUrl());
+            }
+            if (product.getImageUrls() != null) {
+                productExist.setImageUrls(product.getImageUrls());
             }
             if (product.getCategory() != null) {
                 productExist.setCategory(product.getCategory());
@@ -97,18 +118,10 @@ public class ProductServiceImplement implements ProductService {
     }
 
     @Override
-    public void updateStock(Long productId, int quantity) {
-        var product = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found."));
-        product.setStock(quantity);
-        productRepository.save(product);
-    }
-
-    @Override
     public List<Product> findProductsByCart(Long cartId) {
-        var products = productRepository.findProductsByCart(cartId);
-        if (products.isEmpty()) {
-            throw new NoSuchElementException("No products found for cart " + cartId);
+        var cart = cartRepository.findById(cartId);
+        if (cart.isEmpty()) {
+            throw new NoSuchElementException("Cart not found.");
         }
         return productRepository.findProductsByCart(cartId);
     }

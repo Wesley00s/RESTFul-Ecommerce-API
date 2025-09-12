9c6f935cb6713be46cc2f9b64acb13de4dc034fe
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-12 18:39:51 -0300

    refactor: move config files to rest package

--- src/main/java/org/wesley/ecommerce/application/config/rest/AppConfig.java
+++ src/main/java/org/wesley/ecommerce/application/config/rest/AppConfig.java
@@ -1,4 +1,4 @@
-package org.wesley.ecommerce.application.config;
+package org.wesley.ecommerce.application.config.rest;
 
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
--- src/main/java/org/wesley/ecommerce/application/config/rest/CorsConfig.java
+++ src/main/java/org/wesley/ecommerce/application/config/rest/CorsConfig.java
@@ -1,4 +1,4 @@
-package org.wesley.ecommerce.application.config;
+package org.wesley.ecommerce.application.config.rest;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;

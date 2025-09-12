183e303e69e2ff63cb742acb04fa4c170e0c04d2
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-12 18:46:27 -0300

    refactor: remove dotenv config

--- src/main/java/org/wesley/ecommerce/application/Application.java
+++ src/main/java/org/wesley/ecommerce/application/Application.java
@@ -2,13 +2,11 @@ package org.wesley.ecommerce.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.wesley.ecommerce.application.config.DotenvConfig;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        DotenvConfig.loadDotenv();
         SpringApplication.run(Application.class, args);
     }
 

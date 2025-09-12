4671b43ad362ed8b9d5274b2b9e7a96eb4f3fcfe
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-12 18:43:38 -0300

    feat: implement sorting and filtering in find all products

--- src/main/java/org/wesley/ecommerce/application/controller/ProductController.java
+++ src/main/java/org/wesley/ecommerce/application/controller/ProductController.java
@@ -2,18 +2,19 @@ package org.wesley.ecommerce.application.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.wesley.ecommerce.application.controller.dto.ApiResponse;
-import org.wesley.ecommerce.application.controller.dto.PaginationResponse;
-import org.wesley.ecommerce.application.controller.dto.ProductRequestDTO;
-import org.wesley.ecommerce.application.controller.dto.ProductResponseDTO;
+import org.wesley.ecommerce.application.controller.dto.response.ApiResponse;
+import org.wesley.ecommerce.application.controller.dto.response.PaginationResponse;
+import org.wesley.ecommerce.application.controller.dto.request.ProductRequest;
+import org.wesley.ecommerce.application.controller.dto.response.ProductResponse;
 import org.wesley.ecommerce.application.domain.enumeration.ProductCategory;
+import org.wesley.ecommerce.application.domain.enumeration.ProductSortBy;
+import org.wesley.ecommerce.application.domain.enumeration.SortDirection;
 import org.wesley.ecommerce.application.domain.model.Product;
 import org.wesley.ecommerce.application.service.ProductService;
 
@@ -31,8 +32,11 @@ public class ProductController {
     final private ProductService productService;
 
     @PostMapping
-    @Operation(summary = "Create a new product", description = "Create a new product and return the created product's data")
-    public ResponseEntity<Product> createProduct(@RequestBody ProductRequestDTO product) {
+    @Operation(
+            summary = "Create a new product",
+            description = "Create a new product and return the created product's data"
+    )
+    public ResponseEntity<Product> createProduct(@RequestBody ProductRequest product) {
         var productToCreate = productService.create(product.from(randomCode()));
         var location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -43,8 +47,11 @@ public class ProductController {
     }
 
     @PostMapping("/list")
-    @Operation(summary = "Save an list of products", description = "Create an list of products")
-    public ResponseEntity<ArrayList<Product>> createProducts(@RequestBody List<ProductRequestDTO> products) {
+    @Operation(
+            summary = "Save an list of products",
+            description = "Create an list of products"
+    )
+    public ResponseEntity<List<Product>> createProducts(@RequestBody List<ProductRequest> products) {
         var prods = new ArrayList<Product>();
         products.forEach(product -> prods.add(productService.create(product.from(randomCode()))));
         var location = ServletUriComponentsBuilder
@@ -55,18 +62,27 @@ public class ProductController {
     }
 
     @GetMapping
-    @Operation(summary = "Get all products", description = "Retrieve a list of all registered products")
-    public ResponseEntity<ApiResponse<ProductResponseDTO>> getAllProducts(
+    @Operation(
+            summary = "Get all products",
+            description = "Retrieve a list of all registered products"
+    )
+    public ResponseEntity<ApiResponse<ProductResponse>> getAllProducts(
+            @RequestParam(name = "sortBy", required = false) ProductSortBy sortBy,
+            @RequestParam(name = "sortDirection", required = false) SortDirection sortDirection,
+            @RequestParam(name = "name", required = false) String name,
             @RequestParam(name = "page", defaultValue = "0") Integer page,
             @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize
     ) {
-        var products = productService.findAll(page, pageSize);
+        var products = productService.findAll(sortBy, sortDirection, name, page, pageSize);
         return getApiResponseResponseEntity(products);
     }
 
     @GetMapping("/category/{category}")
-    @Operation(summary = "Get products by category", description = "Retrieve a list of products filtered by category")
-    public ResponseEntity<ApiResponse<ProductResponseDTO>> getProductsByCategory(
+    @Operation(
+            summary = "Get products by category",
+            description = "Retrieve a list of products filtered by category"
+    )
+    public ResponseEntity<ApiResponse<ProductResponse>> getProductsByCategory(
             @PathVariable ProductCategory category,
             @RequestParam(name = "page", defaultValue = "0") Integer page,
             @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize
@@ -76,49 +92,53 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Get a product by ID", description = "Retrieve a specific product based on its ID")
-    public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id) {
+    @Operation(
+            summary = "Get a product by ID",
+            description = "Retrieve a specific product based on its ID"
+    )
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
         var product = productService.findById(id);
         return ResponseEntity.ok(
-                new ProductResponseDTO(
-                product.getId(),
-                product.getName(),
-                product.getImageUrl(),
-                product.getCode(),
-                product.getDescription(),
-                product.getStock(),
-                product.getCategory(),
-                product.getPrice()
-        ));
+                ProductResponse.fromDTO(product)
+        );
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Remove product", description = "Remove product by ID")
-    public ResponseEntity<ProductResponseDTO> deleteProduct(@PathVariable Long id) {
+    @Operation(
+            summary = "Remove product",
+            description = "Remove product by ID"
+    )
+    public ResponseEntity<ProductResponse> deleteProduct(@PathVariable Long id) {
         var productToDelete = productService.findById(id);
         productService.delete(productToDelete);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Update a product", description = "Update the data of an existing user based on its ID")
-    public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable Long id, @RequestBody ProductResponseDTO productResponseDTO) {
-        var updatedProduct = productService.update(id, productResponseDTO.from(randomCode()));
-        return ResponseEntity.ok(ProductResponseDTO.fromDTO(updatedProduct));
+    @Operation(
+            summary = "Update a product",
+            description = "Update the data of an existing user based on its ID"
+    )
+    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody ProductResponse productResponse) {
+        var updatedProduct = productService.update(id, productResponse.from(randomCode()));
+        return ResponseEntity.ok(ProductResponse.fromDTO(updatedProduct));
     }
 
     @GetMapping("/cart/{cartId}")
-    @Operation(summary = "Get product from a cart", description = "Retrieve a list of products from a cart")
+    @Operation(
+            summary = "Get product from a cart",
+            description = "Retrieve a list of products from a cart"
+    )
     public ResponseEntity<List<Product>> getProductsByCart(@PathVariable Long cartId) {
         var products = productService.findProductsByCart(cartId);
-        return ResponseEntity.status(HttpStatus.FOUND).body(products);
+        return ResponseEntity.ok(products);
     }
 
     @NotNull
-    private ResponseEntity<ApiResponse<ProductResponseDTO>> getApiResponseResponseEntity(Page<Product> products) {
-        List<ProductResponseDTO> prods = new ArrayList<>(List.of());
+    private ResponseEntity<ApiResponse<ProductResponse>> getApiResponseResponseEntity(Page<Product> products) {
+        List<ProductResponse> prods = new ArrayList<>(List.of());
         for (var product : products) {
-            prods.add(ProductResponseDTO.fromDTO(product));
+            prods.add(ProductResponse.fromDTO(product));
         }
         var pageResponse = new ApiResponse<>(
                 prods,

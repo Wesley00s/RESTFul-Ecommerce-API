b371bf1cbc9c4896ccc2a418f986bb84f4074f92
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-14 17:42:01 -0300

    feat: implement image upload to Cloudinary for products

--- src/main/java/org/wesley/ecommerce/application/service/implement/ProductServiceImplement.java
+++ src/main/java/org/wesley/ecommerce/application/service/implement/ProductServiceImplement.java
@@ -1,10 +1,14 @@
 package org.wesley.ecommerce.application.service.implement;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.wesley.ecommerce.application.controller.dto.request.UpdateProductRequest;
+import org.wesley.ecommerce.application.controller.dto.request.CreateProductRequest;
 import org.wesley.ecommerce.application.domain.enumeration.ProductCategory;
 import org.wesley.ecommerce.application.domain.enumeration.ProductSortBy;
 import org.wesley.ecommerce.application.domain.enumeration.SortDirection;
@@ -15,23 +19,40 @@ import org.wesley.ecommerce.application.service.ProductService;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
+
+import static org.wesley.ecommerce.application.utility.CodeGenerate.randomCode;
 
 @Service
 @AllArgsConstructor
 public class ProductServiceImplement implements ProductService {
     final private ProductRepository productRepository;
     private final CartRepository cartRepository;
+    private final CloudinaryService cloudinaryService;
 
     @Override
-    public Product create(Product product) {
+    @Transactional
+    public Product create(CreateProductRequest request, MultipartFile coverImageFile, List<MultipartFile> otherImageFiles) {
+        CloudinaryService.UploadResult coverUploadResult = cloudinaryService.uploadFile(coverImageFile);
+        Product product = new Product();
+        product.setName(request.name());
+        product.setDescription(request.description());
+        product.setCoverImageUrl(coverUploadResult.url());
+        product.setCoverImagePublicId(coverUploadResult.publicId());
+        product.setCategory(request.category());
+        product.setPrice(request.price());
+        product.setStock(request.stock());
+        product.setCode(randomCode());
+
+        product.getImageUrls().put(coverUploadResult.publicId(), coverUploadResult.url());
+
+        if (otherImageFiles != null && !otherImageFiles.isEmpty()) {
+            List<CloudinaryService.UploadResult> otherImagesResults = cloudinaryService.uploadFiles(otherImageFiles);
+            for (var result : otherImagesResults) {
+                product.getImageUrls().put(result.publicId(), result.url());
+            }
+        }
 
         return productRepository.save(product);
-    }
-
-    @Override
-    public void create(List<Product> products) {
-        productRepository.saveAll(products);
     }
 
     @Override
@@ -73,38 +94,48 @@ public class ProductServiceImplement implements ProductService {
         return product.getStock() >= quantity;
     }
 
+    @Transactional
     @Override
-    public Product update(Long id, Product product) {
-        Optional<Product> productOptional = productRepository.findById(id);
-        if (productOptional.isPresent()) {
-            Product productExist = productOptional.get();
+    public Product update(Long id, UpdateProductRequest request, MultipartFile newCoverImage, List<MultipartFile> newImageFiles) {
+        Product productExist = productRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Product not found for update"));
 
-            if (product.getName() != null) {
-                productExist.setName(product.getName());
-            }
-            if (product.getPrice() != null) {
-                productExist.setPrice(product.getPrice());
-            }
-            if (product.getCoverImageUrl() != null) {
-                productExist.setCoverImageUrl(product.getCoverImageUrl());
-            }
-            if (product.getImageUrls() != null) {
-                productExist.setImageUrls(product.getImageUrls());
-            }
-            if (product.getCategory() != null) {
-                productExist.setCategory(product.getCategory());
-            }
-            if (product.getDescription() != null) {
-                productExist.setDescription(product.getDescription());
-            }
-            if (product.getStock() != null) {
-                productExist.setStock(product.getStock());
-            }
+        if (request.publicIdsToDelete() != null && !request.publicIdsToDelete().isEmpty()) {
+            for (String publicId : request.publicIdsToDelete()) {
+                cloudinaryService.deleteImage(publicId);
+                productExist.getImageUrls().remove(publicId);
 
-            return productRepository.save(productExist);
-        } else {
-            throw new NoSuchElementException("Product not found for update");
+                if (publicId.equals(productExist.getCoverImagePublicId())) {
+                    productExist.setCoverImageUrl(null);
+                    productExist.setCoverImagePublicId(null);
+                }
+            }
         }
+
+        if (newCoverImage != null && !newCoverImage.isEmpty()) {
+            if (productExist.getCoverImagePublicId() != null) {
+                cloudinaryService.deleteImage(productExist.getCoverImagePublicId());
+            }
+            var coverUploadResult = cloudinaryService.uploadFile(newCoverImage);
+            productExist.setCoverImageUrl(coverUploadResult.url());
+            productExist.setCoverImagePublicId(coverUploadResult.publicId());
+            productExist.getImageUrls().put(coverUploadResult.publicId(), coverUploadResult.url());
+        }
+
+        if (newImageFiles != null && !newImageFiles.isEmpty()) {
+            var uploadResults = cloudinaryService.uploadFiles(newImageFiles);
+            for (var result : uploadResults) {
+                productExist.getImageUrls().put(result.publicId(), result.url());
+            }
+        }
+
+        if (request.name() != null) productExist.setName(request.name());
+        if (request.price() != null) productExist.setPrice(request.price());
+        if (request.category() != null) productExist.setCategory(request.category());
+        if (request.description() != null) productExist.setDescription(request.description());
+        if (request.stock() != null) productExist.setStock(request.stock());
+
+        return productRepository.save(productExist);
     }
 
     @Override
@@ -112,6 +143,12 @@ public class ProductServiceImplement implements ProductService {
 
         if (productRepository.existsById(product.getId())) {
             productRepository.deleteById(product.getId());
+            cloudinaryService.deleteImage(product.getCoverImagePublicId());
+            for (var imageUrl : product.getImageUrls().keySet()) {
+                cloudinaryService.deleteImage(imageUrl);
+            }
+            cloudinaryService.deleteImage(product.getCoverImagePublicId());
+
         } else {
             throw new NoSuchElementException("Product with id " + product.getId() + " not found for delete");
         }

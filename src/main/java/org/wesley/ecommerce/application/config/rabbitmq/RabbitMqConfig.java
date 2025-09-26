da3281a1f3fc2418bc493ca58437de3c0db944ba
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-26 20:00:18 -0300

    feat: Add `RabbitMqConfig.java`

---
+++ src/main/java/org/wesley/ecommerce/application/config/rabbitmq/RabbitMqConfig.java
@@ -0,0 +1,14 @@
+package org.wesley.ecommerce.application.config.rabbitmq;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}

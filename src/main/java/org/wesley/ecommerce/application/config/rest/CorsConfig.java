5e16f49d4044e49be7da666e36a4fee1d488eef0
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-26 20:01:11 -0300

    refactor: Refactor `CorsConfig.java`

--- src/main/java/org/wesley/ecommerce/application/config/rest/CorsConfig.java
+++ src/main/java/org/wesley/ecommerce/application/config/rest/CorsConfig.java
@@ -10,10 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.10.102:8090")
-                .allowedOrigins("http://192.168.10.102:4200")
-                .allowedOrigins("http://192.168.0.113:8090")
-                .allowedOrigins("http://192.168.0.113:4200")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true);

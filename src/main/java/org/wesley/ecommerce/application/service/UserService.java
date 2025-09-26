5da3ccb40d700c9e4e1e516aaf6424faca3d7f2c
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-26 20:06:43 -0300

    update: Change imports

--- src/main/java/org/wesley/ecommerce/application/service/UserService.java
+++ src/main/java/org/wesley/ecommerce/application/service/UserService.java
@@ -3,9 +3,8 @@ package org.wesley.ecommerce.application.service;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
-import org.wesley.ecommerce.application.controller.dto.response.AuthResponse;
-import org.wesley.ecommerce.application.controller.dto.request.UserRequest;
-import org.wesley.ecommerce.application.controller.dto.response.LoginResponse;
+import org.wesley.ecommerce.application.api.v1.controller.dto.request.UserRequest;
+import org.wesley.ecommerce.application.api.v1.controller.dto.response.LoginResponse;
 import org.wesley.ecommerce.application.domain.model.Users;
 
 import java.util.UUID;

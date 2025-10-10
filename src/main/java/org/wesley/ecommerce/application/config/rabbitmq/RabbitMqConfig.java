28e2b867ce0b179cfd69373d6179b67a824c163c
Wesley00s <wesley300rodrigues@gmail.com>
2025-10-10 10:24:14 -0300

    feat: Add RabbitMQ configuration for product rating updated queue

--- src/main/java/org/wesley/ecommerce/application/config/rabbitmq/RabbitMqConfig.java
+++ src/main/java/org/wesley/ecommerce/application/config/rabbitmq/RabbitMqConfig.java
@@ -1,5 +1,6 @@
 package org.wesley.ecommerce.application.config.rabbitmq;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +8,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMqConfig {
 
+    public static final String PRODUCT_RATING_UPDATED_QUEUE = "product.rating.updated.queue";
+
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue productRatingUpdatedQueue() {
+        return new Queue(PRODUCT_RATING_UPDATED_QUEUE);
     }
 }

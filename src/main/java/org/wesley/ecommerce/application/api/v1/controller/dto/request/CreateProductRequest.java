063c3d2e306c7cb3f16126d86005a8dab8651883
Wesley00s <wesley300rodrigues@gmail.com>
2025-10-10 10:16:29 -0300

    refactor: Update CreateProductRequest to use categoryId instead of ProductCategory

--- src/main/java/org/wesley/ecommerce/application/api/v1/controller/dto/request/CreateProductRequest.java
+++ src/main/java/org/wesley/ecommerce/application/api/v1/controller/dto/request/CreateProductRequest.java
@@ -1,25 +1,11 @@
 package org.wesley.ecommerce.application.api.v1.controller.dto.request;
 
-import org.wesley.ecommerce.application.domain.enumeration.ProductCategory;
-import org.wesley.ecommerce.application.domain.model.Product;
-
 
 public record CreateProductRequest(
         String name,
         String description,
         Integer stock,
-        ProductCategory category,
+        Long categoryId,
         Double price
 ) {
-
-    public Product from(String code) {
-        var product = new Product();
-        product.setName(name());
-        product.setDescription(description());
-        product.setStock(stock());
-        product.setCategory(category());
-        product.setPrice(price());
-        product.setCode(code);
-        return product;
-    }
 }
